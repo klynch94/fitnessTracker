8a3c4af73c61965eae2d93da6bf574172cf51594
Kieran Lynch <lynch.kieran1@gmail.com>
2020-09-08 21:24:57 -0500

    created HTML routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const router = require("express").Router();
+
+// homepage
+router.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
+// exercise page
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+})
+
+// stats page
+router.get("/stats", (req, res)=> {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+})
+
+module.exports = router;
