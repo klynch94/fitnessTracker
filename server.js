34fb43496dab0bbd5e97c9f33710952f27d7b3ef
Kieran Lynch <lynch.kieran1@gmail.com>
2020-09-08 21:04:29 -0500

    created server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+// install packages
+const express = require("express");
+const mongoose = require("mongoose");
+// set port
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// routes
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", 
+    { 
+        useNewUrlParser: true,
+    }
+);
+
+app.listen(PORT, () => console.log("Connected"));
